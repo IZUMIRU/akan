ecbfd83f4503f2d0421d1e1818d0b54db6fd70c0
akihide.egashira <shingashi0624@gmail.com>
2020-02-02 14:38:06 +0900

    (fix) commented swiper

--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -27,15 +27,15 @@ const Home = () => {
         >
           {Demo.map((item, index) => (
             <Card key={index}>
-              {/* <CardItem
+              <CardItem
                 image={item.image}
                 name={item.name}
                 description={item.description}
                 matches={item.match}
                 actions
-                onPressLeft={() => this.swiper.swipeLeft()}
-                onPressRight={() => this.swiper.swipeRight()}
-              /> */}
+                // onPressLeft={() => this.swiper.swipeLeft()}
+                // onPressRight={() => this.swiper.swipeRight()}
+              />
             </Card>
           ))}
         </CardStack>
