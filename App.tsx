626497ea07598ee8be2014c23d9864c7a1e1a014
akihide.egashira <shingashi0624@gmail.com>
2020-02-02 12:36:41 +0900

    (feat) added login-screen plugin via yarn

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>akan application v0.1.1(alpha)</Text>
     </View>
   );
 }
@@ -12,8 +12,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
