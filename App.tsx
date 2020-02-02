a2672bf58f9a8d7865d18145e05db1c0c444ff43
akihide.egashira <shingashi0624@gmail.com>
2020-02-02 13:31:31 +0900

    (feat) added react-navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+// import styles from "./assets/styles";
+// import HomeScreen from "./containers/Home";
+// import MatchesScreen from "./containers/Matches";
+// import MessagesScreen from "./containers/Messages";
+// import ProfileScreen from "./containers/Profile";
+// import Icon from "./components/Icon";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>akan application v0.1.1(alpha)</Text>
-    </View>
-  );
-}
+const App = createBottomTabNavigator(
+	// {
+	// 	Explore: {
+	// 		screen: HomeScreen,
+	// 		navigationOptions: {
+	// 			tabBarIcon: ({ focused }) => {
+	// 				const iconFocused = focused ? "#7444C0" : "#363636";
+	// 				return (
+	// 					<Text style={[styles.iconMenu, { color: iconFocused }]}>
+	// 						<Icon name="explore" />
+	// 					</Text>
+	// 				);
+	// 			}
+	// 		}
+	// 	},
+	// 	Matches: {
+	// 		screen: MatchesScreen,
+	// 		navigationOptions: {
+	// 			tabBarIcon: ({ focused }) => {
+	// 				const iconFocused = focused ? "#7444C0" : "#363636";
+	// 				return (
+	// 					<Text style={[styles.iconMenu, { color: iconFocused }]}>
+	// 						<Icon name="heart" />
+	// 					</Text>
+	// 				);
+	// 			}
+	// 		}
+	// 	},
+	// 	Chat: {
+	// 		screen: MessagesScreen,
+	// 		navigationOptions: {
+	// 			tabBarIcon: ({ focused }) => {
+	// 				const iconFocused = focused ? "#7444C0" : "#363636";
+	// 				return (
+	// 					<Text style={[styles.iconMenu, { color: iconFocused }]}>
+	// 						<Icon name="chat" />
+	// 					</Text>
+	// 				);
+	// 			}
+	// 		}
+	// 	},
+	// 	Profile: {
+	// 		screen: ProfileScreen,
+	// 		navigationOptions: {
+	// 			tabBarIcon: ({ focused }) => {
+	// 				const iconFocused = focused ? "#7444C0" : "#363636";
+	// 				return (
+	// 					<Text style={[styles.iconMenu, { color: iconFocused }]}>
+	// 						<Icon name="user" />
+	// 					</Text>
+	// 				);
+	// 			}
+	// 		}
+	// 	}
+	// },
+	// {
+	// 	tabBarOptions: {
+	// 		activeTintColor: "#7444C0",
+	// 		inactiveTintColor: "#363636",
+	// 		labelStyle: {
+	// 			fontSize: 14,
+	// 			textTransform: "uppercase",
+	// 			paddingTop: 10
+	// 		},
+	// 		style: {
+	// 			backgroundColor: "#FFF",
+	// 			borderTopWidth: 0,
+	// 			paddingVertical: 30,
+	// 			height: 60,
+	// 			marginBottom: 0,
+	// 			shadowOpacity: 0.05,
+	// 			shadowRadius: 10,
+	// 			shadowColor: "#000",
+	// 			shadowOffset: { height: 0, width: 0 }
+	// 		}
+	// 	}
+	// }
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+export default createAppContainer(App);
