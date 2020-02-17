09319643403561b8771e9cb25b2ae1982732bdbf
akihide.egashira <shingashi0624@gmail.com>
2020-02-17 23:02:33 +0900

    (fix) commented react-navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text } from "react-native";
-import createAppContainer from "react-navigation"
-import createBottomTabNavigator from "react-navigation";
+// import createAppContainer from "react-navigation"
+// import createBottomTabNavigator from "react-navigation";
 import styles from "./assets/css/styles";
 import HomeScreen from "./src/container/Home";
 // import MatchesScreen from "./src/containers/Matches";
